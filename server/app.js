cdfec79bbfdc7e1ca6cc3e2981136559bef1bb9f
Gabe Nuñez <gabriel@zinusinc.com>
2020-09-23 20:34:47 -0700

    Working mail fetcher

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const port =  process.env.PORT || 1818;
+const notifier = require('mail-notifier');
 
 app.use(express.json());
 
@@ -17,3 +19,18 @@ app.listen(port, () => {
         console.log(`API listening on port ${port}!`)
     }
 });
+
+const emailNotifier = notifier({
+    user: process.env.EMAIL_USERNAME,
+    password: process.env.EMAIL_PASSWORD,
+    host: process.env.EMAIL_HOST,
+    port: 993,
+    tls: true,
+    tlsOptions: { rejectUnauthorized: false }
+});
+
+emailNotifier.on('end', () => emailNotifier.start())
+  .on('mail', mail => {
+      console.log(mail);
+    })
+  .start();
