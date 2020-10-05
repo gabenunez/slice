5e1c4207123bd53c985d2fc7c41ab16405599112
Gabe Nuñez <gabriel@zinusinc.com>
2020-10-04 22:43:04 -0700

    Really messy db stuff for heroku

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,19 @@
 const { Pool } = require('pg');
-const pool = new Pool();
 
-module.exports = {
-  query: (text, params) => pool.query(text, params)
+if (process.env.NODE_ENV === 'production') {
+  const connectionString = process.env.DATABASE_URL;
+
+  const pool = new Pool({
+    connectionString: connectionString,
+  });
+
+  module.exports = {
+    query: (text, params) => pool.query(text, params)
+  }
+} else {
+  const pool = new Pool();
+
+  module.exports = {
+    query: (text, params) => pool.query(text, params)
+  }
 }
