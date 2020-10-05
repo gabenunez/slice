bbe5080c9c44d008d467817d8c5ea45b8e54936e
Gabe Nuñez <gabriel@zinusinc.com>
2020-10-04 21:35:20 -0700

    Add require for path

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const port =  process.env.PORT || 1818;
+const path = require('path');
 const notifier = require('mail-notifier');
 const { addEmailtoDB } = require('./components/database');
 
